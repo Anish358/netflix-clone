7dfb9a48317886d253a35a88a71635af785b2938
Anish358 <anishejawale@gmail.com>
2022-03-31 12:33:26 +0530

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyB5dGNyt4o1-ECIV1rW3F61a_IlWfEr5go",
+//     authDomain: "netflix-clone-eff79.firebaseapp.com",
+//     projectId: "netflix-clone-eff79",
+//     storageBucket: "netflix-clone-eff79.appspot.com",
+//     messagingSenderId: "229393520549",
+//     appId: "1:229393520549:web:217778f660a151f96e3a4c"
+//   };
